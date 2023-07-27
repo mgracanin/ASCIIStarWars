893225f2471716e471342d263c08c0165334b647
Matija Gračanin <35041118+mgracanin@users.noreply.github.com>
2023-07-27 22:51:36 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from asyncio import sleep
 from pathlib import Path
-import os, sys
+import sys
 
 '''
-ASCII Art from https://www.asciimation.co.nz/
+ASCII Art (Star Wars) from https://www.asciimation.co.nz/
 '''
 
 starwars_file_path = (f"{Path(__file__).parent}/starwars.txt")
@@ -11,15 +11,16 @@ starwars_file_path = (f"{Path(__file__).parent}/starwars.txt")
 async def main():
     LPS = 14 ## Lines per frame
 
-    if os.path.exists(starwars_file_path):
+    try:
         with open(starwars_file_path, 'r') as f:
-            podaci = f.read().split('\n')
+                podaci = f.read().split('\n')
         print('\n' * LPS)
-    else:
-        print(f"File {starwars_file_path} does not exist. Make sure file is in the same directory as this script.")
+    except FileNotFoundError:
+        print(f"File {starwars_file_path} does not exist. \nMake sure file is in the same directory as this script and try again.")
         sys.exit(1)
 
-    for i in range(0, len(podaci), LPS):
+    ## Each frame is 67 columns by 14 rows
+    for i in range(0, len(podaci), LPS): 
         print("\x1b[{}A\x1b[J{}".format(LPS, '\n'.join(podaci[i + 1:i + LPS])))  # \x1b[{}A\x1b[J move ESC char 14 lines
         await sleep(int(podaci[i]) * 67 / 1000) ## Delay = 67
 
