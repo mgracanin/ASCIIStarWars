4cd87183ae4744f59d491d09bc059ea5da858af1
Matija Gračanin <35041118+mgracanin@users.noreply.github.com>
2023-07-27 22:54:34 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ async def main():
         print(f"File {starwars_file_path} does not exist. \nMake sure file is in the same directory as this script and try again.")
         sys.exit(1)
 
-    ## Each frame is 67 columns by 14 rows
+    ## Each frame is 67 columns by 14 rows, so interating the file in chunks of LPS lines
     for i in range(0, len(podaci), LPS): 
         print("\x1b[{}A\x1b[J{}".format(LPS, '\n'.join(podaci[i + 1:i + LPS])))  # \x1b[{}A\x1b[J move ESC char 14 lines
         await sleep(int(podaci[i]) * 67 / 1000) ## Delay = 67
